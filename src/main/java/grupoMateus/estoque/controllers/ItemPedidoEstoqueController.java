4b14a75292e2f1c011dcf6d15d694e2f8461ac82
Arianne <ariannedossantos.f.e@hotmail.com>
2019-03-30 12:55:11 -0300

    adicão do diagrama ER-PDF e controller ItemPedidoEstoque

--- src/main/java/grupoMateus/estoque/controllers/ItemPedidoEstoqueController.java
+++ src/main/java/grupoMateus/estoque/controllers/ItemPedidoEstoqueController.java
@@ -1,4 +1,44 @@
 package grupoMateus.estoque.controllers;
 
+import grupoMateus.estoque.models.ItemPedidoEstoque;
+import grupoMateus.estoque.repositories.ItemPedidoEstoqueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class ItemPedidoEstoqueController {
+
+    @Autowired
+    private ItemPedidoEstoqueRepository itemPedidoEstoqueRepository;
+
+
+    @RequestMapping(value = "/itens-pedidos-estoques", method = RequestMethod.POST)
+    public void salvar(@RequestBody ItemPedidoEstoque itemPedidoEstoque){
+        itemPedidoEstoqueRepository.save(itemPedidoEstoque);
+    }
+
+    @RequestMapping(value = "/itens-pedidos-estoques")
+    public Object buscar(){
+        return itemPedidoEstoqueRepository.findAll();
+    }
+
+    @RequestMapping(value = "/itens-pedidos-estoques/{id}", method = RequestMethod.PUT)
+    public void atualizar(@PathVariable("id") Long id, @RequestBody ItemPedidoEstoque itemPedidoEstoque) throws Exception{
+
+        ItemPedidoEstoque itemPedidoEstoque1 = itemPedidoEstoqueRepository.findById(id).get();
+
+        if( itemPedidoEstoque1 == null){
+            throw new Exception("/itens-pedidos-estoques não encontrado!");
+        }
+
+        itemPedidoEstoque1.setQuantidade(itemPedidoEstoque.getQuantidade());
+
+        itemPedidoEstoqueRepository.save(itemPedidoEstoque1);
+    }
+
+
+    @RequestMapping(value = "/itens-pedidos-estoques/{id}", method = RequestMethod.DELETE)
+    public void deletar(@PathVariable("id") Long id){
+        itemPedidoEstoqueRepository.deleteById(id);
+    }
 }
